e658f4cb3e7c95b8cb9318a97494a357c7ed9d0a
Sam <dokladal.samuel.me@gmail.com>
2024-12-11 22:01:04 +0100

    Create Learn_1.cpp

---
+++ Learn_1.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+
+int main() {
+    
+int age = 21;
+int year = 2023;
+int day = 7.5;
+
+double price = 10.99;
+double gpa = 2.5;
+double temperature = 25.1;
+
+//single character
+
+char grade = 'A';
+char intial = 'B';
+
+//boolean (true / flase)
+bool student = true;
+bool power = true;
+bool forsale = true;
+
+//string
+
+std::string name = "Bro";
+
+std ::cout << "hello " << name << '\n';
+std::cout << " you are " << age << " years old";
+std::cout << student;
+std::cout << student << '\n' << power << '\n' << forsale << '\n';
+std::cout << intial << '\n' << grade << '\n';
+
+
+
+
+    return 0;
+}
